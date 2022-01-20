fe9f3c0a6ba25be2ee9235e169ddbaf2d9f9932f
zalfanuramalia <zalfafanur@gmail.com>
2022-01-20 17:00:37 +0700

    file tugas javascript lanjutan

---
+++ task1.js
@@ -0,0 +1,32 @@
+const cekHariKerja = (day) => {
+    return new Promise((resolve, reject) =>{
+        setTimeout(()=>{
+            const dataDay = ['senin','selasa','rabu','kamis','jumat']
+            let cek = dataDay.find((item)=>{
+                return item === day
+            })
+            if(cek){
+                resolve(cek)
+            } 
+            else {
+                reject(new Error('Hari ini bukan hari kerja'))
+            }
+        }, 3000)
+    })
+}
+
+//cekHariKerja('rabu').then((cek)=>{
+//    console.log("Hari " + cek + " adalah hari kerja")
+//}).catch((err)=>{
+//    console.log(""+ err + "" )
+//})
+
+async function proses(){
+    try{
+        const result = await cekHariKerja('minggu')
+        console.log("Hari " + result + " adalah hari kerja")
+    }catch(err){
+        console.log(""+err)
+    }
+}
+proses()
