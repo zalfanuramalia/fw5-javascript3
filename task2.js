59095890dfe95c42f3104d9b863780bc5ab9ccb2
zalfanuramalia <zalfafanur@gmail.com>
2022-01-20 18:31:55 +0700

    file tugas javascript lanjutan

--- task2.js
+++ task2.js
@@ -1,13 +1,23 @@
 const getmonth = (callback) => {
     setTimeout(()=>{
         let error = false;
-        let month = ['Januari','February','March','April','May','June','July','August',
-        'September','October','November','Desember'];
+        let month = ['January','February','March','April','May','June','July','August',
+        'September','October','November','December'];
         if (!error){
             callback(null, month)
         }
         else {
-            callback(new Error('Sorry Data Not Found', []))
+            callback(new Error('Sorry Data Not Found'), [])
         }
     }, 4000)
 }
+function printMonth(err,month){
+	if(month.length){
+      month.map((mon) =>{
+        console.log(mon);
+      })
+    }else{
+      console.log(""+err)
+    }
+  }
+  getmonth(printMonth)
