43d160eec67680dcfbc5cac4184dd4dc22cbd4eb
zalfanuramalia <zalfafanur@gmail.com>
2022-01-21 12:30:16 +0700

    file tugas javascript lanjutan

--- task3.js
+++ task3.js
@@ -1,27 +1,44 @@
-function panggilKucing(name){
-    return new Promise(function(resolve, reject){
-      setTimeout(function(){
-        resolve(name+' : Meow..');
-      }, 2000);
-    });
+//Program 1
+
+let ditepati = true;
+const janji1 = new Promise((resolve, reject) => {
+  if ( ditepati) {
+    setTimeout (()=> {
+      resolve ("Janji telah ditepati");
+    }, 2000)
+  } else {
+    reject ("Ingkar janji..");
   }
-  
-  function callKucing(name){
-    setTimeout(function(){
-      return name+' : Purr';
-    }, 2000);
-  }
-  
-  async function main(){
-    let dialogTanpaPromise = callKucing('Kitty');
-    console.log(dialogTanpaPromise);
-  
-    try{
-      let dialogDenganPromise = await panggilKucing('Doraemon');
-      console.log(dialogDenganPromise);
-    } catch(error){
-      console.log(error);
+});
+
+janji1.then((item) => {
+  console.log("OK! :" + item)
+}).catch((err) => {
+  console.log("NOT OK! :" + err)
+})
+
+//Program 2
+
+function promise() {
+  return new Promise ((resolve, reject) => {
+    const time = 6000
+    if (time < 5000 ){
+      setTimeout((cek)=>{
+        resolve(cek)
+      }, time);
+    } else {
+      reject ("Kelamaan!")
     }
+  })
+}
+
+async function janji(){
+  try {
+    const coba = await promise();
+    console.log("selesai!");
+  } catch (err) {
+    console.log(err);
   }
-  
-  main();
+}
+
+janji()
